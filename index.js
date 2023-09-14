fbc635ee8f8ea830ed094a31cc231e7a72971b89
ABDULLAH KHAN <98159659+Abdulester@users.noreply.github.com>
2023-09-14 06:31:26 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import inquirer from 'inquirer';
+const systemGeneratedNo = Math.floor(Math.random() * 10);
+console.log(systemGeneratedNo);
+const answer = await inquirer.prompt([
+    {
+        type: "number",
+        name: "userGuess",
+        message: "Write Your Guess b/w 1 to 10:",
+    }
+]);
+const { userGuess } = answer;
+console.log(userGuess, "userGuess", systemGeneratedNo, "systemGeneratedNo");
+if (userGuess === systemGeneratedNo) {
+    console.log("Yeaaaa Your answer is correct \n You Win!");
+}
+else {
+    console.log("Please try again Better luck next time!");
+}
